2805429f68fc42468b3687f4ab99f2ad90fc7df3
Alexey <49400470+piy-bebe@users.noreply.github.com>
2026-03-02 10:52:20 +0300

    feat: добавить кнопки для информации

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -3,7 +3,10 @@ import style from './Footer.module.scss';
 export default function Footer() {
   return (
     <div className={style.container}>
-      <div className={style.wrap}>Тут добавлю чутка позже</div>
+      <div className={style.wrap}>
+        <button className={style.buttonText}>Реквизиты компании</button>
+        <button className={style.buttonText}>Политика конфиденциальности</button>
+      </div>
     </div>
   );
 }
