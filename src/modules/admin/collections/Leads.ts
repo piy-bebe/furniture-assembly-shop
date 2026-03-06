ebbcb64f0c94929a1145a6beadb9e65029b3da26
piy-bebe <piy.bebe@gmail.com>
2026-03-06 10:16:32 +0300

    feat: добавить логику отправки на почту

--- src/modules/admin/collections/Leads.ts
+++ src/modules/admin/collections/Leads.ts
@@ -1,7 +1,36 @@
+import nodemailer from 'nodemailer';
 import type { CollectionConfig } from 'payload';
 
 const Leads: CollectionConfig = {
   slug: 'leads',
+  hooks: {
+    afterChange: [
+      async ({ doc, operation }) => {
+        if (operation === 'create') {
+          const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: 465,
+            secure: true,
+            auth: {
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASS,
+            },
+          });
+
+          await transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to: process.env.EMAIL_TO,
+            subject: 'Новая заявка с сайта',
+            html: `
+              <h2>Новая заявка</h2>
+              <p><b>Имя:</b> ${doc.name}</p>
+              <p><b>Телефон:</b> ${doc.phone}</p>
+            `,
+          });
+        }
+      },
+    ],
+  },
   admin: {
     useAsTitle: 'name',
     defaultColumns: ['name', 'phone', 'createdAt'],
